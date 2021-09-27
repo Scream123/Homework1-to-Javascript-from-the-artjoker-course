22c9d31d224e2924809e32691dcae96a440748fb
Volodymyr Marakhovskyi <Fly_life@mail.ru>
2021-09-27 11:57:12 +0300

    Update exponential.js

--- exponential.js
+++ exponential.js
@@ -1,14 +1,19 @@
-document.addEventListener('DOMContentLoaded', function () {
+    document.addEventListener('DOMContentLoaded', function () {
 
     function exponential(number, degree) {
-
+            let message = '';
         let exponent = 1;
-        for (let i = 1; i <= degree; i++) {
-            exponent *= number;
-        }
-        return "Число " + number + " в " + degree + "-ой степени: " + Number(exponent);
-    }
 
-    document.getElementsByClassName("header-exponential")[0].innerText += "Возведение в степень: ";
-    document.getElementsByClassName('exponential-result')[0].innerHTML = exponential(2, 5) + "<br/>";
+        if (!Number.isInteger(degree) || degree <= 0) {
+            message = " Параметр " + degree + "должен быть числом и больше нуля<br/>"
+        } else {
+            for (let i = 1; i <= degree; i++) {
+                message = "Число " + number + " в " + degree + "-ой степени: " + Number(exponent);
+                exponent *= number;
+            }
+        }
+
+      return document.getElementsByClassName('exponential-result')[0].innerHTML = message + "<br/>";
+    }
+        exponential(2, 0);
 });
